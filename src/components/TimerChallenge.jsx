6c5bd17470116de682c6ed3550901c44dc956a7f
Mohd Salim <saalim9965@gmail.com>
2024-07-13 18:15:25 +0530

    section 8: lecture 142 more examples when to use refs and state

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,25 +2,44 @@ import { useRef, useState } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ title, targetTime }) {
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  //   const [timerStarted, setTimerStarted] = useState(false);
+  //   const [timerExpired, setTimerExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
   const timer = useRef();
   const dialog = useRef();
 
-  function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      dialog.current.open();
-    }, targetTime * 1000);
+  //   function handleStart() {
+  //     timer.current = setTimeout(() => {
+  //       setTimerExpired(true);
+  //       dialog.current.open();
+  //     }, targetTime * 1000);
 
-    setTimerStarted(true);
+  //     setTimerStarted(true);
+  //   }
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    setTimeRemaining(targetTime * 1000);
+    dialog.current.open();
+  }
+
+  function handleStart() {
+    timer.current = setInterval(() => {
+      setTimeRemaining((previousTimeRemaining) => previousTimeRemaining - 1000);
+    }, 1000);
   }
 
   console.log("Timer challenge loading...");
 
+  //   function handleStop() {
+  //     clearTimeout(timer.current); // will not trigger TimerChallenge (this component) re-rendering
+  //   }
+
   function handleStop() {
-    clearTimeout(timer.current); // will not trigger TimerChallenge (this component) re-rendering
+    clearInterval(timer.current); // will not trigger TimerChallenge (this component) re-rendering
+    dialog.current.open();
   }
   return (
     <>
@@ -28,17 +47,16 @@ export default function TimerChallenge({ title, targetTime }) {
       {/* since it is invisible in the dom by default */}
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You Lost!</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={timerStarted ? "active" : undefined}>
-          {timerStarted ? "Time is running..." : "Timer inactive"}
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running..." : "Timer inactive"}
         </p>
       </section>
     </>
