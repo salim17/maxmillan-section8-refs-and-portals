d5cfa1b10a99a9defdf6f769f4142f74bf29632a
Mohd Salim <saalim9965@gmail.com>
2024-07-13 12:00:04 +0530

    section 8: lecture 137 setting timer and managing state

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
     <>
       <Player />
       <div id="challenges">
-        <TimerChallenge title="Easy" targetTime={1}/>
-        <TimerChallenge title="Not easy" targetTime={5}/>
-        <TimerChallenge title="Getting tough" targetTime={10}/>
-        <TimerChallenge title="Pros only" targetTime={15}/>
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not easy" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
       </div>
     </>
   );
