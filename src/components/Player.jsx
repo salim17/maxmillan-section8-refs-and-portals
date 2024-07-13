407e7c6034ba304097ee3467804de9ffae0413e9
Mohd Salim <saalim9965@gmail.com>
2024-07-13 11:35:31 +0530

    section 8: lecture 135 refs vs state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,9 +5,16 @@ export default function Player() {
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
   const playerName = useRef();
 
+  // key difference in state vs refs.
+  // whenever state changes component functions re-executes(re-renders) but in refs it does not.
+
+  // therefore state should be used for values that are directly reflected in the UI and should not be used
+  // for "behind the scenes" values that have no direct UI impact
+
+  // Refs can be used to gain direct DOM element access (great for reading values or accessing certain browser APIs)
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
-    playerName.current.value = null; //do not do it like this unless it is simple usecase like this here
+    playerName.current.value = ""; //do not do it like this unless it is simple usecase like this here
   }
   return (
     <section id="player">
