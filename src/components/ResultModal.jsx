1659c1e3331d77cf13cc7ecf1ec265c02d878c56
Mohd Salim <saalim9965@gmail.com>
2024-07-13 18:44:26 +0530

    section 8: lecture 144 enhancing the demo app result modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -4,13 +4,14 @@ import { forwardRef, useImperativeHandle, useRef } from "react";
 // hence we need to use forwardRef function from react
 // forwardRef recieves ref as the second argument, first argument is props
 const forwardedModal = forwardRef(function ResultModal(
-  { targetTime, remainingTime, handleReset },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
 
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   // this hook is used to uncouple the tight binding of this component with ref.
   // now using this we can change dialog html element to a div.
@@ -26,6 +27,7 @@ const forwardedModal = forwardRef(function ResultModal(
   return (
     <dialog className="result-modal" ref={dialog}>
       {userLost && <h2>You lost </h2>}
+      {!userLost && <h2>Your Score: {score} </h2>}
       <p>
         The target time was <strong>{targetTime} seconds</strong>
       </p>
@@ -33,8 +35,8 @@ const forwardedModal = forwardRef(function ResultModal(
         You stopped the timer with{" "}
         <strong>{formattedRemainingTime} seconds left</strong>
       </p>
-      <form method="dialog">
-        <button onSubmit={handleReset}>Close</button>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
       </form>
     </dialog>
   );
