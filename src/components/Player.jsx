6aa6384a3fab76f10a8c8d68273512eb00bb04ec
Mohd Salim <saalim9965@gmail.com>
2024-07-13 11:19:29 +0530

    section 8: lecture 134 manipulating the dom with refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -7,12 +7,13 @@ export default function Player() {
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = null; //do not do it like this unless it is simple usecase like this here
   }
   return (
     <section id="player">
-      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input ref={playerName} type="text"/>
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
