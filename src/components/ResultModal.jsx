e9d51e20f9117ba790d29d7a39b6f8cca2c9c463
Mohd Salim <saalim9965@gmail.com>
2024-07-13 17:13:32 +0530

    section 8: lecture 141 exposing component API via the useImperativeHandle hook

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,11 +1,27 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 // Refs cannot be forwarded in the form or props
 // hence we need to use forwardRef function from react
 // forwardRef recieves ref as the second argument, first argument is props
-const forwardedModal =  forwardRef(function ResultModal({ result, targetTime }, ref) {
+const forwardedModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
+  const dialog = useRef();
+
+  // this hook is used to uncouple the tight binding of this component with ref.
+  // now using this we can change dialog html element to a div.
+  // and now we can call this open method from outside.
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
-    <dialog className="result-modal" ref={ref}>
+    <dialog className="result-modal" ref={dialog}>
       <h2>You {result} </h2>
       <p>
         The target time was <strong>{targetTime} seconds</strong>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -11,35 +11,36 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
 
     setTimerStarted(true);
   }
 
-  console.log('Timer challenge loading...')
+  console.log("Timer challenge loading...");
 
   function handleStop() {
     clearTimeout(timer.current); // will not trigger TimerChallenge (this component) re-rendering
   }
   return (
     <>
-    <ResultModal ref={dialog} targetTime={targetTime} result="lost" /> {/* since it is invisible in the dom by default */}
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You Lost!</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? "Stop" : "Start"} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? "active" : undefined}>
-        {timerStarted ? "Time is running..." : "Timer inactive"}
-      </p>
-    </section>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />{" "}
+      {/* since it is invisible in the dom by default */}
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p>You Lost!</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Time is running..." : "Timer inactive"}
+        </p>
+      </section>
     </>
   );
 }
